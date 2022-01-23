b160dc867ca8b8483e435ed1cae4e9ba5bd7eb89
AmirHossein_Enayati <eamirh19@outlook.com>
2022-01-23 20:54:43 +0330

    2

--- test.py
+++ test.py
@@ -1,9 +1,52 @@
+from flask import Response
+from flask import request
+from flask import Flask
+import json
 import requests
+import os
 url = "https://api.telegram.org/bot5164303840:AAFinWpqK_Nk3_6ZJscImsaL31zoCE0dsyo/"
 
+app = Flask(__name__)
 
-data_tel = {
-    'chat_id': 107814146,
-    'text': 'Salam khobi?'
-}
-requests.post(url+'sendMessage', data_tel)
+
+def get_all_updates():
+    response = requests.get(url+'getUpdates')
+    return response.json()
+
+
+def get_last_update(allUpdates):
+    return allUpdates['result'][-1]
+
+
+def get_chat_id(update):
+    return update['message']['chat']['id']
+
+
+def sendmessage(chat_id, text):
+    sendData = {'chat_id': chat_id, 'text': text}
+    response = requests.post(url + 'sendMessage', sendData)
+    return response
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+
+        # get last interact
+        msg = request.get_json()
+
+        # get chat id of last interact
+        chat_id = get_chat_id(msg)
+
+        # get user's input
+        text = msg['message'].get('text', '')
+
+        if text == '/start':
+            sendmessage(chat_id, 'Name Film Ra Vared Konid')
+        return Response('ok', status=200)
+    else:
+        return ''
+
+
+app.run('0.0.0.0', port=int(os.environ.get('PORT', 5000)))
